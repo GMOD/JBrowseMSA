add8b9ba1ae7b6131812c25bf600d879c2d7246a
Colin <colin.diesh@gmail.com>
2026-03-24 15:35:29 -0400

    Updates

--- assets/fetchUtils-iNLkv-IC.js
+++
@@ -1,2 +0,0 @@
-async function s(t,e){const n=await fetch(t,e);if(!n.ok)throw new Error(`HTTP ${n.status} fetching ${t} ${await n.text()}`);return n}async function o(t,e){return(await s(t,e)).text()}async function r(t,e){return(await s(t,e)).json()}function a(t){return new Promise(e=>setTimeout(e,t))}export{a,r as j,o as t};
-//# sourceMappingURL=fetchUtils-iNLkv-IC.js.map
