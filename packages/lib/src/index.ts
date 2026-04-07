8f93107f7c3930aa61b1b0b0d38b2febbcc9402e
Colin <colin.diesh@gmail.com>
2026-04-07 11:56:14 -0400

    Export HierarchyNode type to fix app build (TS2883)

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,6 +1,7 @@
 export { default as MSAView } from './components/Loading.tsx'
 export { type MsaViewModel, default as MSAModelF } from './model.ts'
 export type { MSAParserType } from 'msa-parsers'
+export type { HierarchyNode } from './hierarchy.ts'
 export type { InterProScanResults } from './launchInterProScan.ts'
 export type {
   Accession,
