b54d9e24a9a12e3c92d6cb9d2cff6fafe213283c
Colin <colin.diesh@gmail.com>
2026-04-15 22:43:05 -0400

    Updates

---
+++ assets/LoadingEllipses-BTidtcgJ.js
@@ -0,0 +1,15 @@
+import{t as e}from"./jsx-runtime-BbRADC1s.js";import{Nt as t,n,p as r,r as i}from"./Close-D4GsDTZb.js";var a=e(),o=i()({dots:{"&::after":{display:`inline-block`,content:`""`,width:`1em`,textAlign:`left`,animation:`${t`
+      0% {
+        content: '';
+      }
+      25% {
+          content: '.';
+      }
+      50% {
+        content: '..';
+      }
+      75% {
+        content: '...';
+      }
+      `} 1.2s infinite ease-in-out`}}});function s({message:e,children:t,variant:i=`body2`,...s}){let{classes:c}=o();return(0,a.jsx)(r,{className:n(c.dots,s.className),...s,variant:i,children:e||`Loading`})}export{s as t};
+//# sourceMappingURL=LoadingEllipses-BTidtcgJ.js.map
